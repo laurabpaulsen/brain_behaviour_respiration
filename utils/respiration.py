3611c7805a5becc30b00fe56ce2b051e4d519197
laurabpaulsen <202005791@post.au.dk>
2024-08-29 14:55:39 +0200

    fix constant misspelling of trough

--- utils/respiration.py
+++ utils/respiration.py
@@ -36,43 +36,43 @@ def extract_phase_angle(resp_timeseries:np.arrray, figpath = None):
     # normalize the interpolated time series
     normalised_ts = (r - np.nanmean(r)) / np.nanstd(r)
 
-    # finding peaks and throughs
+    # finding peaks and troughs
     peaks = signal.find_peaks(normalised_ts)[0]
-    throughs = np.array([], dtype = int)
+    troughs = np.array([], dtype = int)
 
-    for peak1, peak2 in zip(peaks, peaks[1:]):                  # finding the throughs -> the minimum between the peaks
+    for peak1, peak2 in zip(peaks, peaks[1:]):                  # finding the troughs -> the minimum between the peaks
         tmp_resp = normalised_ts[peak1:peak2]                   # respiration time course between the peaks
-        through_ind_tmp = np.where(tmp_resp == min(tmp_resp))
-        through_ind = int(through_ind_tmp[0] + peak1)                # get the index relative to the entire time series
+        trough_ind_tmp = np.where(tmp_resp == min(tmp_resp))
+        trough_ind = int(trough_ind_tmp[0] + peak1)                # get the index relative to the entire time series
 
-        throughs = np.append(throughs, through_ind) 
+        troughs = np.append(troughs, trough_ind) 
 
     # calculate the phase angle
     phase_angle = np.zeros(len(normalised_ts))
     phase_angle[:] = np.nan # fill with nans
     
-    # set throughs to pi and peaks to 0
-    phase_angle[throughs], phase_angle[peaks] = np.pi, 0
+    # set troughs to pi and peaks to 0
+    phase_angle[troughs], phase_angle[peaks] = np.pi, 0
 
-    # interpolate the phase angle between peaks and throughs
-    for peak1, peak2, through in zip(peaks, peaks[1:], throughs):
-        phase_angle[peak1:through] = np.linspace(0 + np.pi/(through-peak1), np.pi,  through-peak1)
-        phase_angle[through:peak2] = np.linspace(-np.pi + np.pi/(peak2-through), 0, peak2-through)
+    # interpolate the phase angle between peaks and troughs
+    for peak1, peak2, trough in zip(peaks, peaks[1:], troughs):
+        phase_angle[peak1:trough] = np.linspace(0 + np.pi/(trough-peak1), np.pi,  trough-peak1)
+        phase_angle[trough:peak2] = np.linspace(-np.pi + np.pi/(peak2-trough), 0, peak2-trough)
 
     if figpath:
-        sanity_check_phase_angle(resp_timeseries, normalised_ts, peaks, throughs, phase_angle, figpath)
+        sanity_check_phase_angle(resp_timeseries, normalised_ts, peaks, troughs, phase_angle, figpath)
 
-    return normalised_ts, peaks, throughs, phase_angle
+    return normalised_ts, peaks, troughs, phase_angle
 
 
 
-def sanity_check_phase_angle(resp_timeseries, normalised_ts, peaks, throughs, phase_angle, savepath):
+def sanity_check_phase_angle(resp_timeseries, normalised_ts, peaks, troughs, phase_angle, savepath):
     fig, ax = plt.subplots(1, 1, figsize = (12, 6), dpi = 300)
     ax.plot(resp_timeseries, label = "original timeseries")
     ax.plot(normalised_ts, label = "normalised interpolated timeseries")
     ax.plot(phase_angle, label = "phase_angle", color = "green")
     ax.scatter(peaks, np.zeros(len(peaks)), s = 4, label = "peaks")
-    ax.scatter(throughs, np.zeros(len(throughs)), s = 4, label = "throughs")
+    ax.scatter(troughs, np.zeros(len(troughs)), s = 4, label = "troughs")
 
     ax.legend()
 
