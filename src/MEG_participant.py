3e2bd26cef8974916bdd32a35e1f18c8bd0adc19
laurabpaulsen <202005791@post.au.dk>
2025-03-17 11:03:28 +0100

    small updates

--- src/MEG_participant.py
+++ src/MEG_participant.py
@@ -2,16 +2,20 @@ import mne
 from mne.minimum_norm import apply_inverse, make_inverse_operator
 from pathlib import Path
 import respiration as resp
+import pickle as pkl
+from config import event_ids
 
 
 class MEG_participant:
-    def __init__(self, subj_id, meg_date, mr_date, bad_channels = [], run_path = Path("."),  project_path = Path("/projects/MINDLAB2021_MEG-CerebellarClock-FuncSig")):
+    def __init__(self, subj_id, meg_date, mr_date, event_ids:dict, bad_channels = [], run_path = Path("."), project_path = Path("/projects/MINDLAB2021_MEG-CerebellarClock-FuncSig"), n_jobs = 2):
         self.subj_id = subj_id
         self.bad_channels = bad_channels
+        self.event_ids = event_ids
         self.meg_date = meg_date
         self.mr_date = mr_date
         self.project_path = project_path
         self.run_path = run_path
+        self.n_jobs = n_jobs
         self.fnames = {}
 
     def populate_fnames(self, mkdirs = True):
@@ -25,7 +29,8 @@ class MEG_participant:
         self.fnames["resp"] = self.fnames["scratch"] / "respiration" / self.subj_id
         self.fnames["subj_freesurfer"] = self.fnames["scratch"] / "freesurfer" / self.subj_id
         self.fnames["subj_bem"] = self.fnames["subj_freesurfer"] / "bem"
-        self.fnames["phase_angles"] = self.fnames["resp"] / f"{self.subj_id}_phase_angles_events.csv"
+        self.fnames["phase_angles_events"] = self.fnames["resp"] / f"{self.subj_id}_phase_angles_events.csv"
+        self.fnames["phase_angles_ts"] = self.fnames["resp"] / f"{self.subj_id}_phase_angles.pkl"
 
         files_with_number = list((self.fnames["raw"] / self.subj_id / self.meg_date / "MEG").rglob("*raw_[0-9].fif"))
         files_without_number = list((self.fnames["raw"] / self.subj_id / self.meg_date / "MEG").rglob("*raw.fif"))
@@ -73,12 +78,13 @@ class MEG_participant:
             preload=True,
         )
 
-    def epochs_extract_power_sourcespace(self, labels = ['Cerebelum_Crus1_R'], freqs =):
+    """
+    def epochs_extract_power_sourcespace(self, labels = ['Cerebelum_Crus1_R'], freqs = None):
 
         inverse_operator = make_inverse_operator(self.raw.info, fwd, noise_cov, depth=None, loose=loose, verbose=True)
         
         labels_power = mne.norm.source_induced_power(
-            epochs,
+            self.epochs,
             inverse_operator,
             freqs,
             labels,
@@ -89,33 +95,38 @@ class MEG_participant:
             return_plv=False,
         )
         pass
-
-    def extract_resp_angle(self):        
+    """
+    def extract_resp_angle(self, l_freq = None, h_freq = 10, resp_ch_name = "MISC001", sample_rate = 300):        
         # check if raw is loaded, if not:
         if not self.raw.preload: # only load the respiratory channel
-            resp_ts = self.raw.copy().pick("MISC001")
+            resp_ts = self.raw.copy().pick(resp_ch_name)
             resp_ts.load_data()
         
         else: # get the respiration data
-            resp_ts = self.raw.copy().pick("MISC001")
+            resp_ts = self.raw.copy().pick(resp_ch_name)
 
-        resp_ts = resp_ts.filter(None, 1, picks = "MISC001")
-        #resp_ts, tmp_events = resp_ts.resample(100, events = self.events) # resampling the events at the same time!
+        resp_ts = resp_ts.filter(l_freq, h_freq, picks = resp_ch_name, n_jobs = self.n_jobs)
+        resp_ts, tmp_events = resp_ts.resample(sample_rate, events = self.events) # resampling the events at the same time!
+
+        first_sample = resp_ts.first_samp
+
+        tmp_events[:, 0] = tmp_events[:, 0]-first_sample
 
         resp_ts = resp_ts.get_data().squeeze() # squeeze to get rid of the channel dimension
-        normalised_ts, peaks, troughs, phase_angle = resp.extract_phase_angle(resp_ts, widths=250, min_sample = 50)
+        normalised_ts, peaks, troughs, phase_angle = resp.extract_phase_angle(resp_ts, widths=100, min_sample = 50)
 
 
-        df = resp.phase_angle_events(phase_angle, self.events)
+        df = resp.phase_angle_events(phase_angle, tmp_events, hz = 300, event_ids = self.event_ids)
 
-        df.to_csv(self.fnames["phase_angles"], index = False)
-
-        # NOTE: maybe also save the phase vector!
+        df.to_csv(self.fnames["phase_angles_events"], index = False)
         
         resp.sanity_check_phase_angle(resp_timeseries = None, normalised_ts = normalised_ts, peaks = peaks, troughs = troughs, phase_angle = phase_angle, savepath = self.fnames["fig"] / f"{self.subj_id}_respiration.png")
         resp.summary_plots(peaks, troughs, phase_angle, savepath = self.fnames["fig"] / f"{self.subj_id}_respiration_summary.png" )
 
 
+        # save phase_angle as pickle
+        with open(self.fnames["phase_angles_ts"], 'wb') as fn:
+            pkl.dump(phase_angle, fn, protocol=-1)
 
 
 
